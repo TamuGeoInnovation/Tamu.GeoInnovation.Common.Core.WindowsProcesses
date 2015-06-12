facae0e448d2e9460e8ba5bb4c99334ec29b530e
dwgoldbe <daniel.goldberg@tamu.edu>
2015-06-12 16:23:16 -0500

    Initial Commit

---
+++ Src/Main/ShellCommands/ShellCommandUtils.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+
+namespace USC.GISResearchLab.Common.Core.ShellCommands
+{
+
+    // from http://www.codeproject.com/Articles/25983/How-to-Execute-a-Command-in-C
+
+    public class ShellCommandUtils
+    {
+
+        /// <summary>
+        /// Executes a shell command synchronously.
+        /// </summary>
+        /// <param name="command">string command</param>
+        /// <returns>string, as output of the command.</returns>
+        public static void ExecuteCommandSync(object command)
+        {
+            try
+            {
+                // create the ProcessStartInfo using "cmd" as the program to be run,
+                // and "/c " as the parameters.
+                // Incidentally, /c tells cmd that we want it to execute the command that follows,
+                // and then exit.
+                System.Diagnostics.ProcessStartInfo procStartInfo =
+                    new System.Diagnostics.ProcessStartInfo("cmd", "/c " + command);
+
+                // The following commands are needed to redirect the standard output.
+                // This means that it will be redirected to the Process.StandardOutput StreamReader.
+                procStartInfo.RedirectStandardOutput = true;
+                procStartInfo.UseShellExecute = false;
+                // Do not create the black window.
+                procStartInfo.CreateNoWindow = true;
+                // Now we create a process, assign its ProcessStartInfo and start it
+                System.Diagnostics.Process proc = new System.Diagnostics.Process();
+                proc.StartInfo = procStartInfo;
+                proc.Start();
+                // Get the output into a string
+                string result = proc.StandardOutput.ReadToEnd();
+                // Display the command output.
+                Console.WriteLine(result);
+            }
+            catch (Exception objException)
+            {
+                throw new Exception("Error in ExecuteCommandSync: " + objException.Message, objException);
+            }
+        }
+
+
+        /// <summary>
+        /// Execute the command Asynchronously.
+        /// </summary>
+        /// <param name="command">string command.</param>
+        public static void ExecuteCommandAsync(string command)
+        {
+            try
+            {
+                //Asynchronously start the Thread to process the Execute command request.
+                Thread objThread = new Thread(new ParameterizedThreadStart(ExecuteCommandSync));
+                //Make the thread as background thread.
+                objThread.IsBackground = true;
+                //Set the Priority of the thread.
+                objThread.Priority = ThreadPriority.AboveNormal;
+                //Start the thread.
+                objThread.Start(command);
+            }
+            catch (ThreadStartException objException)
+            {
+                throw new Exception("Error in ExecuteCommandSync - ThreadStartException: " + objException.Message, objException);
+            }
+            catch (ThreadAbortException objException)
+            {
+                throw new Exception("Error in ExecuteCommandSync - ThreadAbortException: " + objException.Message, objException);
+            }
+            catch (Exception objException)
+            {
+                throw new Exception("Error in ExecuteCommandSync: " + objException.Message, objException);
+            }
+        }
+    }
+}
